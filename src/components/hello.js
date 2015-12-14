681035916e996ce57b8df6f09d4aa8e8dae65d07
Guy Kremnitzer <guykr@stratoscale.com>
2015-12-14 21:31:53 +0200

    selection

--- src/components/hello.js
+++ src/components/hello.js
@@ -1,9 +1,88 @@
 import React from 'react';
+import {Table, Column, Cell} from 'fixed-data-table';
+
+const rowCount = 1000;
+
+const getData = () => {
+  const res = [];
+
+  for (let i = 0; i < rowCount; i++) {
+    res.push([`a${i}`, `b${i}`, `c${i}`]);
+  };
+
+  return res;
+};
+
+const rows = getData();
+
+class MyCell extends React.Component {
+  render() {
+    const {data, selected, ...props} = this.props;
+    return (
+      <Cell {...props} className={ selected ? 'selected' : null }>
+        {data}
+      </Cell>
+    );
+  }
+}
 
 export default class Hello extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedIndex: [],
+    };
+  };
+
+  handleClick = (rowIndex) => {
+    const copy = this.state.selectedIndex.slice(0);
+    const index = copy.indexOf(rowIndex);
+    if (index > -1) {
+      copy.splice(index, 1);
+    } else {
+      copy.push(rowIndex);
+    };
+
+    this.setState({
+      selectedIndex: copy,
+    });
+  };
+
   render() {
     return (
-      <h1 className="cblue">Hello World</h1>
-    )
+      <div>
+      <Table
+      rowHeight={50}
+      rowsCount={rows.length}
+      width={600}
+      height={5000}
+      headerHeight={50}>
+      <Column
+        header={<Cell>Col 1</Cell>}
+        cell={({rowIndex, ...props}) => (
+          <MyCell {...props} data={rows[rowIndex][0]} selected={this.state.selectedIndex.indexOf(rowIndex) > -1} onClick={() => this.handleClick(rowIndex)}>
+          </MyCell>
+        )}
+        width={200}
+      />
+      <Column
+        header={<Cell>Col 2</Cell>}
+        cell={({rowIndex, ...props}) => (
+          <MyCell {...props} data={rows[rowIndex][1]} selected={this.state.selectedIndex.indexOf(rowIndex) > -1} onClick={() => this.handleClick(rowIndex)}>
+          </MyCell>
+        )}
+        width={200}
+      />
+      <Column
+        header={<Cell>Col 3</Cell>}
+        cell={({rowIndex, ...props}) => (
+          <MyCell {...props} data={rows[rowIndex][2]} selected={this.state.selectedIndex.indexOf(rowIndex) > -1} onClick={() => this.handleClick(rowIndex)}>
+          </MyCell>
+        )}
+        width={200}
+      />
+    </Table>
+    </div>
+    );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import './css/style.css';
+import '../node_modules/fixed-data-table/dist/fixed-data-table.css';
 
 import React from 'react';
+import ReactDom from 'react-dom';
 import Hello from './components/hello';
 
-React.render(<Hello />, document.getElementById('root'));
+ReactDom.render(<Hello />, document.getElementById('root'));
